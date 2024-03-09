f90fbc53469a2f47a40ed1df1df11477344bcea6
Naga Mallesh <89628161+Mallesh511@users.noreply.github.com>
2024-03-09 16:55:35 +0530

    Update ErrorController.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/CustomErrorController.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/CustomErrorController.java
@@ -6,15 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CustomErrorController implements ErrorController {
-
-   @RequestMapping("/error")
-   public String handleError() {
-       // Your custom error handling logic goes here
-       return "error"; // Return the name of your custom error page
-   }
-
-   public String getErrorPath() {
-       return "/error";
+public class ErrorController {
+   
+   public static class CustomErrorController {
+      @RequestMapping("/error")
+      public String handleError() {
+          // Your custom error handling logic goes here
+          return "error"; // Return the name of your custom error page
+      }
+   
+      public String getErrorPath() {
+          return "/error";
+      }
    }
 }
